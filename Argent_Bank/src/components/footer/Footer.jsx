2a23d7673c194b3873a721e882ca7316ec8f7b67
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-09-05 09:30:19 +0200

    modification localisation dossier Frontend

---
+++ Argent_Bank/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './footer.scss'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </div>
+  )
+}
