cd1b48bf83ea95f1e722aa2e0d3c3abfe2d768ee
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-09-30 11:07:02 +0200

    Correction sur la page Error404 du lien pour le retour sur la page d'accueil

--- Argent_Bank/src/pages/error/Error.tsx
+++ Argent_Bank/src/pages/error/Error.tsx
@@ -1,11 +1,12 @@
 import './error.scss'
+import { Link } from 'react-router-dom'
 
 export const Error = () => {
   return (
     <div>
         <h1>404</h1>
         <p>Oups ! Cette page est introuvable.</p>
-        <button>Retour à la page d'accueil</button>
+        <Link to="/">Retour à la page d'accueil</Link>
     </div>
   )
 }
